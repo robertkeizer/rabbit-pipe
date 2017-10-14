270e7a420b6b334d7dc33b9ed7b5f233c470df60
Robert Keizer <robert@keizer.ca>
2017-10-14 13:10:37 -0500

    Adding simple tests.

---
+++ test/main.js
@@ -0,0 +1,8 @@
+const Main	= require( "../" );
+const assert	= require( "assert" );
+
+describe( "Main", function( ){
+	it( "Is a function", function( ){
+		assert.ok( typeof( Main ) == "function" );
+	} );
+} );
